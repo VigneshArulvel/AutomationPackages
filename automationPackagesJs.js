151abb9542dce6d1c7a185619e6d388a5f31a823
Vignesh Arulvel <vignesh1466@gmail.com>
2018-12-02 21:49:25 +0530

    form for tool

--- automationPackagesJs.js
+++ automationPackagesJs.js
@@ -1,3 +1,14 @@
 function main (){
     
 }
+function showTextBox(){
+    var ipBox = document.getElementById("ipBox");
+    var checkBoxIp2 = document.getElementById("checkBoxIp2");
+    var checkBoxIp1 = document.getElementById("checkBoxIp1");
+    if (checkBoxIp2.checked == true && checkBoxIp1.checked == true){
+        ipBox.style.display = "block";
+    }
+    else{
+        ipBox.style.display = "none";    
+    }
+}
